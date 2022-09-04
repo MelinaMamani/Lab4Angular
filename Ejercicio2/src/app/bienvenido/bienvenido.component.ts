37bbc947c46ba9f7464b5a9da0407e3830dc3a3a
MelinaFE99 <erikamelinamamani@gmail.com>
2022-09-03 22:39:21 -0300

    Usuario llamado en componente bienvenido

--- Ejercicio2/src/app/bienvenido/bienvenido.component.ts
+++ Ejercicio2/src/app/bienvenido/bienvenido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../entidades/usuario';
 
 @Component({
   selector: 'app-bienvenido',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienvenidoComponent implements OnInit {
 
+  usuario : Usuario = new Usuario();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.usuario.nombre = 'Melina';
+    this.usuario.clave = '';
+    localStorage.setItem('usuario', JSON.stringify(this.usuario));
   }
-
 }
